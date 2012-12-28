3fa5be5f29437305e3d6f25217a33f5cdaf7be80
Cornelius Hald <cornelius.hald@tourschall.com>
2012-12-28 20:05:10 +0100

    Improves FPS from max ~20 to ~50 on my DevAlpha-A.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QtGui/QApplication>
 #include <QtDeclarative>
+#include <QGLWidget>
+#include <QGLFormat>
 #include "qmlapplicationviewer.h"
 #include "servercomm.h"
 #include <QDebug>
@@ -11,7 +13,23 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
     qDebug("CuteSoma started");
     ServerComm sc;
+
+    // GL viewport increases performance on blackberry
+    QGLFormat format = QGLFormat::defaultFormat();
+    format.setSampleBuffers(false);
+    QGLWidget *glWidget = new QGLWidget(format);
+    glWidget->setAutoFillBackground(false);
+
     QmlApplicationViewer viewer;
+    viewer.setViewport(glWidget);
+
+    // More gfx performance
+    viewer.setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+    viewer.setAttribute(Qt::WA_OpaquePaintEvent);
+    viewer.setAttribute(Qt::WA_NoSystemBackground);
+    viewer.viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
+    viewer.viewport()->setAttribute(Qt::WA_NoSystemBackground);
+    
     viewer.rootContext()->setContextProperty("serverComm", &sc);
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/CuteSoma/main.qml"));
